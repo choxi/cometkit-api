cb47cb2e62dbfa076ec6114525671933067b93f0
Roshan Choxi <roshan.choxi@gmail.com>
2017-09-09 14:33:32 -0500

    websocket connection

--- src/app.js
+++ src/app.js
@@ -7,12 +7,23 @@ import jsdoc      from "jsdoc-api"
 import glob       from "glob"
 import Path       from "path"
 import fs         from "fs"
+import expressWs  from "express-ws"
+import helmet     from "helmet"
 
 export default class App {
   constructor() {
     this.router = express()
+    expressWs(this.router)
+
     this.router.use(BodyParser.json())
     this.router.use(cors())
+    this.router.use(helmet())
+    this.router.use(helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'", "*"],
+        connectSrc: ["'self'", "*"]
+      }
+    }))
 
     process.on('unhandledRejection', (reason, p) => {
       console.log('Unhandled Rejection at:', p, 'reason:', reason)
@@ -21,20 +32,44 @@ export default class App {
     if(process.env.NODE_ENV !== "test")
       this.router.use(morgan(process.env.MORGAN_LOG_FORM || 'combined'))
 
-    this.router.post("/docs", (request, response) => {
-      let owner = request.body.owner
-      let repo  = request.body.repo
-
-      let tmpPath
-      if(process.env.NODE_ENV == "production")
-        tmpPath = Path.join("/", "tmp", repo)
-      else
-        tmpPath = Path.join(".", "tmp", repo)
-
-      download(`${owner}/${repo}`, tmpPath, () => {
-        let docs = this.process(tmpPath)
-        response.json(docs)
+    this.router.ws("/ws", (ws, request) => {
+      ws.on("connection", (ws, request) => {
+        console.log("Client connected.")
       })
+
+      ws.on("message", (msg) => {
+        let action, formatted
+
+        try {
+          action    = JSON.parse(msg)
+          formatted = JSON.stringify(action, null, 4)
+        } catch(e) {
+          console.log(`Invalid Action: ${msg}`)
+          return
+        }
+
+        console.log(`Action:\n${formatted}`)
+
+        if(action.type === "CREATE_DOCS") {
+          let owner = action.owner
+          let repo  = action.repo
+          let tmpPath
+
+          if(process.env.NODE_ENV == "production")
+            tmpPath = Path.join("/", "tmp", repo)
+          else
+            tmpPath = Path.join(".", "tmp", repo)
+
+          download(`${owner}/${repo}`, tmpPath, () => {
+            let response = { docs: this.process(tmpPath) }
+            ws.send(JSON.stringify(response))
+          })
+        }
+      })
+    })
+
+    this.router.get("/", (request, response) => {
+      response.send("Comet API")
     })
   }
 
@@ -42,8 +77,6 @@ export default class App {
     let path  = Path.join(directory, "**", "*.{jsx,js}")
 
     let files = glob.sync(path)
-    files     = files.filter(file => !file.match(/component\.jsx$/))
-
     let docs  = jsdoc.explainSync({ files: files })
     docs      = docs.filter(doc => !doc.undocumented)
 
